4a2aaf8165a89ecc7981dd8770ede0cd84d62239
Nicholas Choo Jia Jun <nicholaschoojiajun@gmail.com>
2023-03-11 16:59:05 +0800

    login and registration + others

---
+++ firebase_config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDLIjMRK1nHeVQl2mztHW-J8cXhjKhE5RY",
+    authDomain: "cashflow-50414.firebaseapp.com",
+    databaseURL: "https://cashflow-50414-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "cashflow-50414",
+    storageBucket: "cashflow-50414.appspot.com",
+    messagingSenderId: "1073197981055",
+    appId: "1:1073197981055:web:c2dc6fa838455c084cce4a",
+    measurementId: "G-3R5S557CMH"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
